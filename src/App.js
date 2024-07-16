b9e74c866de2076147f174e9a7b73b729217a04b
Murari Singh <murarisingh847407@gmail.com>
2024-07-16 16:47:37 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import "./App.css";
+import Whats from "./Whats";
+
+function App() {
+  return (
+    <>
+      <div>
+        {/* First Section Page */}
+
+        <div className="w-screen h-screen  overflow-hidden">
+          <img
+            className="w-full h-full"
+            src="https://img.freepik.com/free-photo/cascade-boat-clean-china-natural-rural_1417-1356.jpg?w=826&t=st=1710374892~exp=1710375492~hmac=448c6dd11aa0ab437ae44c8527e66fbee2cf91db6271468f67d76095e5e02f97"
+            alt="photos"
+          />
+          <div className="absolute top-[50%] left-[50%] translate-x-[-50%]">
+            <button className="text-white bg-gray-600 px-5 py-2 tracking-widest text-xl font-bold rounded-lg ">
+            
+              PARALLAX WEBSITE{" "}
+            </button>
+          </div>
+        </div>
+
+        {/* Second Section Page */}
+
+        <div className="max-w-[1300px] mx-auto text-center py-10 my-14 px-6">
+          <h1 className="font-bold text-xl mb-4">Section One</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+            aut amet odio est laborum obcaecati perferendis! Mollitia officiis,
+            consectetur vitae laborum eos expedita reprehenderit nostrum
+            tempore. Atque laborum repudiandae adipisci nihil. Natus cum
+            sapiente culpa excepturi quasi magni quibusdam ipsum accusamus
+            laborum, numquam odit! Nisi repudiandae cumque incidunt tempora
+            explicabo vero in corporis provident quidem quisquam sit, asperiores
+            ullam placeat ut iure pariatur ratione laboriosam totam amet minus.
+            Porro, quam quisquam explicabo rerum labore, tempora officia eveniet
+            mollitia iure a fugiat, magnam at. Vero magni aliquam ea molestias
+            omnis, quia fuga architecto maiores ut magnam! Culpa, cum.
+            Blanditiis, tempore temporibus.
+          </p>
+        </div>
+
+        {/* third section  */}
+
+        <div className=" h-screen  text center flex justify-center items-center">
+          <div className="w-screen h-screen  overflow-x-hidden">
+            <img
+              className="w-full h-full absolute "
+              src="https://img.freepik.com/free-photo/cascade-boat-clean-china-natural-rural_1417-1356.jpg?w=826&t=st=1710374892~exp=1710375492~hmac=448c6dd11aa0ab437ae44c8527e66fbee2cf91db6271468f67d76095e5e02f97"
+              alt="photos"
+            />
+          </div>
+          <div className="absolute">
+            <button className="  text-white bg-gray-600 px-5 py-2 tracking-widest text-xl font-bold rounded-lg ">
+              Explore Nature
+            </button>
+          </div>
+        </div>
+
+        {/* Fourth Section  */}
+        <div className="w-full text-white bg-gray-500 mx-auto text-center py-10  px-32">
+          <h1 className="font-bold text-xl mb-4">Section Two</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+            aut amet odio est laborum obcaecati perferendis! Mollitia officiis,
+            consectetur vitae laborum eos expedita reprehenderit nostrum
+            tempore. Atque laborum repudiandae adipisci nihil. Natus cum
+            sapiente culpa excepturi quasi magni quibusdam ipsum accusamus
+            laborum, numquam odit! Nisi repudiandae cumque incidunt tempora
+            explicabo vero in corporis provident quidem quisquam sit, asperiores
+            ullam placeat ut iure pariatur ratione laboriosam totam amet minus.
+            Porro, quam quisquam explicabo rerum labore, tempora officia eveniet
+            mollitia iure a fugiat, magnam at. Vero magni aliquam ea molestias
+            omnis, quia fuga architecto maiores ut magnam! Culpa, cum.
+            Blanditiis, tempore temporibus.
+          </p>
+        </div>
+        {/* fifth Section  */}
+
+        <div className=" h-screen  text center flex justify-center items-center">
+          <div className="w-screen h-screen  overflow-x-hidden">
+            <img
+              className="w-full h-full absolute "
+              src="https://img.freepik.com/free-photo/cascade-boat-clean-china-natural-rural_1417-1356.jpg?w=826&t=st=1710374892~exp=1710375492~hmac=448c6dd11aa0ab437ae44c8527e66fbee2cf91db6271468f67d76095e5e02f97"
+              alt="photos"
+            />
+          </div>
+          <div className="absolute">
+            <button className="  text-white bg-gray-600 px-5 py-2 tracking-widest text-xl  rounded-lg ">
+              LOOK, DEEP INTO NATURE
+            </button>
+          </div>
+        </div>
+
+        {/* 6TH Section  */}
+        <div className="w-full text-white bg-gray-500 mx-auto text-center py-10  px-32">
+          <h1 className="font-bold text-xl mb-4">Section Three</h1>0
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+            aut amet odio est laborum obcaecati perferendis! Mollitia officiis,
+            consectetur vitae laborum eos expedita reprehenderit nostrum
+            tempore. Atque laborum repudiandae adipisci nihil. Natus cum
+            sapiente culpa excepturi quasi magni quibusdam ipsum accusamus
+            laborum, numquam odit! Nisi repudiandae cumque incidunt tempora
+            explicabo vero in corporis provident quidem quisquam sit, asperiores
+            ullam placeat ut iure pariatur ratione laboriosam totam amet minus.
+            Porro, quam quisquam explicabo rerum labore, tempora officia eveniet
+            mollitia iure a fugiat, magnam at. Vero magni aliquam ea molestias
+            omnis, quia fuga architecto maiores ut magnam! Culpa, cum.
+            Blanditiis, tempore temporibus.
+          </p>
+        </div>
+        {/* Seventh section */}
+        <div className=" h-screen  text center flex justify-center items-center">
+          <div className="w-screen h-screen  overflow-x-hidden">
+            <img
+              className="w-full h-full absolute "
+              src="https://img.freepik.com/free-photo/cascade-boat-clean-china-natural-rural_1417-1356.jpg?w=826&t=st=1710374892~exp=1710375492~hmac=448c6dd11aa0ab437ae44c8527e66fbee2cf91db6271468f67d76095e5e02f97"
+              alt="photos"
+            />
+          </div>
+          <div className="absolute">
+            <button className="  text-white bg-gray-600 px-5 py-2 tracking-widest text-xl  rounded-lg ">
+              DO SOME CODING
+            </button>
+          </div>
+        </div>
+      </div>
+
+     
+<Whats />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Whats.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Whats = () => {
+  return (
+    <div>
+        <a href="https://web.whatsapp.com/"  >Link TO Whatsapp</a>
+    </div>
+  )
+}
+
+export default Whats
